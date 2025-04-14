cd3bd5e1842b6cf464873a79dd6f5cb7986927b1
Urielpereyra15 <urielpereyra883@gmail.com>
2025-04-14 19:31:02 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const cartas = [
+  'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠',
+  'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+  'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+  'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦'
+];
+
+function sacarCarta() {
+  if (cartas.length === 0) {
+    alert("No hay más cartas.");
+    return;
+  }
+  const indice = Math.floor(Math.random() * cartas.length);
+  const cartaSacada = cartas.splice(indice, 1)[0];
+  document.getElementById("pila").innerHTML = `<div class="carta">${cartaSacada}</div>`;
+}
